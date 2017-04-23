5465a3aeef0c1fa72940597235c92467bdd03f52
forthaven-Lu <jason.halflu@gmail.com>
2017-04-23 00:26:01 -0400

    pajama loading/home

--- paja-magic/js/my.js
+++ paja-magic/js/my.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
   // $('.moreinfo').on('click',function(event){
   //   $('.sleeping2').css("margin-top","-10vh");
   // });
-
+  $('.load-wrap').delay(5000).fadeIn(1000);
+$('.start').on('click',function(){
+  $('.loading-page').hide();
+  $('.content').fadeIn(1000);
+});
 
 
   // Click scroll animation
